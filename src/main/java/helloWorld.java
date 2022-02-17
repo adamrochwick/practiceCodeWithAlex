6fcc89cf6a7a5829f5e690968ec972e4e7762e58
adam <rochwick.adam@gmail.com>
2022-02-17 18:44:14 -0500

    Setting up repo, first commit.

---
+++ src/main/java/helloWorld.java
@@ -0,0 +1,11 @@
+public class helloWorld {
+
+    private static String message(String message) {
+        String secretMessage = message;
+        return secretMessage;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(message("Hello World"));
+    }
+}
